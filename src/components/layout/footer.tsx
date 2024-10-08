1b1ce85ec2a3054aca1951cb34ac675dac53fc33
redheet <naufalzidane32@gmail.com>
2024-10-08 14:53:02 +0700

    footer link add fasilitas

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -155,7 +155,7 @@ function Footer() {
 
               <ul className="mt-8 space-y-4 text-sm ">
                 <li>
-                  <a className="text-gray-700 transition hover:text-gray-700/75" href="/">
+                  <a className="text-gray-700 transition hover:text-gray-700/75" href="/fasilitas">
                     {' '}
                     Fasilitas{' '}
                   </a>
